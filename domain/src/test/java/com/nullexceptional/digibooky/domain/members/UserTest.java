ff19606f9f94ca90d15025f12c8e4e20e939ca1b
Akagami1030 <omar.lamrabet@vdab.be>
2020-03-26 16:19:45 +0100

    directory jenkins v9

--- domain/src/test/java/com/nullexceptional/digibooky/domain/members/UserTest.java
+++ domain/src/test/java/com/nullexceptional/digibooky/domain/members/UserTest.java
@@ -3,8 +3,6 @@ package com.nullexceptional.digibooky.domain.members;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserTest {
     @Test
     void dummyTest() {
