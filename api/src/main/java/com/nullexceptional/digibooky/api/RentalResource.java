262df8a406cfd91b149c5b014658a22fabd35a44
Sven Verboven <sven_verboven@hotmail.com>
2020-03-26 12:17:46 +0100

    added RentalResource.java, RentalService.java, Rental.java, RentalRepository.java

---
+++ api/src/main/java/com/nullexceptional/digibooky/api/RentalResource.java
@@ -0,0 +1,9 @@
+package com.nullexceptional.digibooky.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/rentals")
+public class RentalResource {
+}
---
+++ service/src/main/java/com/nullexceptional/digibooky/service/RentalService.java
@@ -0,0 +1,7 @@
+package com.nullexceptional.digibooky.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RentalService {
+}
