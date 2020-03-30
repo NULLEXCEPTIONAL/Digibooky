d2fafdf13515cf58f3557298ee61e25b586f6ca9
Sven Verboven <sven_verboven@hotmail.com>
2020-03-30 20:48:00 +0200

    added tests for all methods

--- api/src/test/java/com/nullexceptional/digibooky/api/RentalResourceTest.java
+++ api/src/test/java/com/nullexceptional/digibooky/api/RentalResourceTest.java
@@ -3,10 +3,14 @@ package com.nullexceptional.digibooky.api;
 import com.nullexceptional.digibooky.domain.book.Author;
 import com.nullexceptional.digibooky.domain.book.Book;
 import com.nullexceptional.digibooky.domain.book.BookRepository;
+import com.nullexceptional.digibooky.domain.book.dto.BookDtoGeneral;
 import com.nullexceptional.digibooky.domain.members.User;
 import com.nullexceptional.digibooky.domain.members.UserRepository;
+import com.nullexceptional.digibooky.domain.rental.Rental;
+import com.nullexceptional.digibooky.domain.rental.RentalRepository;
 import com.nullexceptional.digibooky.domain.rental.dto.CreateRentalDto;
 import com.nullexceptional.digibooky.domain.rental.dto.RentalDto;
+import com.nullexceptional.digibooky.service.book.BookMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +19,7 @@ import reactor.core.publisher.Mono;
 
 import java.nio.charset.Charset;
 import java.time.LocalDate;
-import java.util.Base64;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -30,6 +33,8 @@ class RentalResourceTest {
     private BookRepository bookRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RentalRepository rentalRepository;
 
     private Book book = new Book("9785568123279", "The Sorceror's Stone", new Author("Rowlings", "JK"), "Blabla summary");
     private User user = new User(UUID.randomUUID(), "465321", "Poppy", "Mommy", "poppy@gmail.com", null);
@@ -51,7 +56,7 @@ class RentalResourceTest {
     }
 
     @Test
-    void lendBook_withAuthorization_thenStatusIsOk() {
+    void lendBook_withAuthorization_thenStatusIsOkAndRentalDtoIsOk() {
         // Given
         bookRepository.registerNewBook(book);
         userRepository.saveUser(user);
@@ -76,18 +81,64 @@ class RentalResourceTest {
     }
 
     @Test
-    void name() {
+    void returnBook_withNoAuthorization_thenStatusIsUnAuthorized() {
         // Given
-
+        Rental rental = rentalRepository.saveRental(new Rental(book, user));
+        String url = "/rentals/" + rental.getId();
         // When
-
-        // Then
-
+        webTestClient
+                .patch()
+                .uri(url)
+                .contentType(APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isUnauthorized();
     }
 
     @Test
-    void getAllBooksOverdue_withAuthorization_thenStatusIsOk() {
+    void returnBook_withAuthorizationAndReturnedOnTime_thenStatusIsOkAndReturnedStringMessageIsOk() {
         // Given
+        Rental rental = rentalRepository.saveRental(new Rental(book, user));
+        String url = "/rentals/" + rental.getId();
+        // When
+        webTestClient
+                .patch()
+                .uri(url)
+                .header("Authorization", "Basic " + base64Encode("member:member@gmail.com"))
+                .contentType(APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .value(string -> assertThat(string).isEqualTo("Book is returned on time"));
+    }
+
+    @Test
+    void returnBook_withAuthorizationAndReturnedToLate_thenStatusIsOkAndReturnedStringMessageIsOk() {
+        // Given
+        Rental rental = rentalRepository.saveRental(new Rental(book, user));
+        rental.setStartDate(LocalDate.now().minusMonths(1));
+        String url = "/rentals/" + rental.getId();
+        // When
+        webTestClient
+                .patch()
+                .uri(url)
+                .header("Authorization", "Basic " + base64Encode("member:member@gmail.com"))
+                .contentType(APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .value(string -> assertThat(string).isEqualTo("Book is returned to late, due date was on " + rental.getReturnDate()));
+    }
+
+    @Test
+    void getAllBooksOverdue_withAuthorization_thenStatusIsOkAndReturnedListIsOk() {
+        // Given:
+        List<BookDtoGeneral> expected = Collections.singletonList(new BookMapper().fromBookToBookDtoGeneral(book));
+        Rental rental = new Rental(book, user);
+        rental.setStartDate(LocalDate.now().minusMonths(1));
+        rentalRepository.saveRental(rental);
         String url = "/rentals";
         // When
         webTestClient
@@ -95,7 +146,10 @@ class RentalResourceTest {
                 .uri(url)
                 .header("Authorization", "Basic " + base64Encode("librarian:librarian@gmail.com"))
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus()
+                .isOk()
+                .expectBodyList(BookDtoGeneral.class)
+                .isEqualTo(expected);
     }
 
     @Test
@@ -107,7 +161,39 @@ class RentalResourceTest {
                 .get()
                 .uri(url)
                 .exchange()
-                .expectStatus().isUnauthorized();
+                .expectStatus()
+                .isUnauthorized();
+    }
+
+    @Test
+    void getLentBooksByMember_withNoAuthorization_thenStatusIsUnauthorized() {
+        // Given
+        String url = "/rentals" + user.getId();
+        // When
+        webTestClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus()
+                .isUnauthorized();
+    }
+
+    @Test
+    void getLentBooksByMember_withAuthorization_thenStatusIsOkAndReturnedListIsOk() {
+        // Given
+        List<BookDtoGeneral> expected = Collections.singletonList(new BookMapper().fromBookToBookDtoGeneral(book));
+        rentalRepository.saveRental(new Rental(book, user));
+        String url = "/rentals/" + user.getId();
+        // When
+        webTestClient
+                .get()
+                .uri(url)
+                .header("Authorization", "Basic " + base64Encode("librarian:librarian@gmail.com"))
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(BookDtoGeneral.class)
+                .isEqualTo(expected);
     }
 
 
