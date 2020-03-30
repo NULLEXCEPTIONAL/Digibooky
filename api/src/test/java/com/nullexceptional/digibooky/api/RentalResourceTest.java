6d106db7a7a36c72088bce1df68202f2be40b2f4
Sven Verboven <sven_verboven@hotmail.com>
2020-03-30 15:45:19 +0200

    added RentalResourceTest.java

--- api/src/test/java/com/nullexceptional/digibooky/api/RentalResourceTest.java
+++ api/src/test/java/com/nullexceptional/digibooky/api/RentalResourceTest.java
@@ -3,16 +3,10 @@ package com.nullexceptional.digibooky.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 
 import java.nio.charset.Charset;
 import java.util.Base64;
-
-import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockUser;
-import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RentalResourceTest {
@@ -21,7 +15,6 @@ class RentalResourceTest {
     private WebTestClient webTestClient;
 
     @Test
-    @WithAnonymousUser
     void getAllBooksOverdue() {
         // Given
         String url = "/rentals";
@@ -29,22 +22,10 @@ class RentalResourceTest {
         WebTestClient.ResponseSpec response = this.webTestClient
                 .get()
                 .uri(url)
-                .header("Authorization", "Basic " + base64Encode("librarin:librarian@gmail.com"))
+                .header("Authorization", "Basic " + base64Encode("librarian:librarian@gmail.com"))
                 .exchange();
         // Then
         response.expectStatus().isOk();
-    }
-
-    private ExchangeFilterFunction memberCredentials() {
-        return basicAuthentication("member","member@gmail.com");
-    }
-
-    private ExchangeFilterFunction librarianCredentials() {
-        return basicAuthentication("librarian","librarian@gmail.com");
-    }
-
-    private ExchangeFilterFunction adminCredentials() {
-        return basicAuthentication("admin","admin@gmail.com");
     }
 
     private String base64Encode(String value) {
