4eced49a413e35b2f416fe503c0271f177acde41
Akagami1030 <omar.lamrabet@vdab.be>
2020-03-31 09:02:18 +0200

    email and inss verification +librarian + tests domain and service

--- api/src/main/java/com/nullexceptional/digibooky/api/UserResource.java
+++ api/src/main/java/com/nullexceptional/digibooky/api/UserResource.java
@@ -42,9 +42,9 @@ public class UserResource {
     @PostMapping( path="/librarian",consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDto createLibrarian(@Valid @RequestBody CreateUserDto createUserDto) {
-        isValidEmailAddress(createUserDto.getEmail());
-        isEmailUnique(createUserDto);
-        isInssNotUnique(createUserDto);
+        userService.checkInssDuplication(createUserDto.getInss());
+        userService.checkIfEmailIsUnique(createUserDto.getEmail());
+        userService.checkIfEmailIsValid(createUserDto.getEmail());
         UserDto librarian = new UserDto(createUserDto.getInss(), createUserDto.getFirstName(), createUserDto.getLastName(), createUserDto.getEmail(), createUserDto.getAddress());
         librarian.setRole(Role.Librarian);
         userService.addUser(librarian);
@@ -66,29 +66,5 @@ public class UserResource {
         return userService.getAllUsers();
     }
 
-    private void isInssNotUnique(CreateUserDto createUserDto) {
-        boolean isInssUnique = userService.getAllUsers().stream()
-                .anyMatch(user -> user.getInss().equals(createUserDto.getInss()));
-        if (isInssUnique) {
-            throw new DuplicationInssException("This inss number already exists. This is a custom message");
-        }
-    }
-
-    private void isValidEmailAddress(String email) {
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-        java.util.regex.Matcher m = p.matcher(email);
-        if (!m.matches()) {
-            throw new ApiEmailRequestException("E-mail is not valid. This is a custom exception");
-        }
-    }
-
-    private void isEmailUnique(CreateUserDto createUserDto) {
-        boolean isEmailUnique = userService.getAllUsers().stream()
-                .anyMatch(x -> x.getEmail().equals(createUserDto.getEmail()));
-        if (isEmailUnique) {
-            throw new ApiEmailDuplicationExceptionRequest("E-mail already exists in our system. Of course this is a custom message");
-        }
-    }
 
 }
